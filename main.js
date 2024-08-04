7a7a05c2876f027f34793c3e640713942aa9ec7f
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-04 20:47:51 +0200

    bg

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ https://sprig.hackclub.com/gallery/getting_started
 const p1 = "1";
 const p2 = "2";
 const apple = "A";
+const grass = "g";
+const grassWithStraws = "G";
 
 setLegend(
     [
@@ -72,6 +74,46 @@ setLegend(
 3333333333333333
 3333333333333333
 3333333333333333`,
+    ],
+    [
+        grass,
+        bitmap`
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444`,
+    ],
+    [
+        grassWithStraws,
+        bitmap`
+4444444444444444
+4444444444444444
+444444D444444DD4
+4444444444444444
+444D44444DD44444
+4444DD44DD444444
+44444DD4D4444444
+44444DD444444444
+44D444DD444DD444
+44DD44DD44DD4444
+444D44DD44D44444
+444D44DD4DD44444
+4444444D4DD44444
+444444444DD44444
+4444444444444444
+4444444444444444`,
     ]
 );
 
@@ -80,22 +122,22 @@ setSolids([p1, p2]);
 let level = 0;
 const levels = [
     map`
-....................
-....................
-....................
-....................
-....................
-....................
-....................
-....................
-....................
-....................
-....................
-....................
-....................
-....................
-....................
-....................`,
+ggggggGggggggggggggg
+gggGgggggggGgggGggGg
+gGggggggggGggggggggg
+ggggGggggggggggggggg
+ggggggggggggggGggggG
+GgggggggGggggGgggggg
+gggggGggggggGggggGgg
+ggggggggGggggggggggg
+ggggggggGggGgggggggg
+gggGgGggggggGggGgggg
+GggggggggGggGggggGgg
+ggggggGggggggggggggg
+gggggGggggggGggggggg
+gGgGgggGggGggggggGgg
+gggggggggGggggGggggg
+gggggggggggggggggggG`,
 ];
 
 setMap(levels[level]);
@@ -155,38 +197,61 @@ const update = setInterval(() => {
     const tilesOnP1NewHead = getTile(p1NewHead[0], p1NewHead[1]);
     if (
         tilesOnP1NewHead.length > 0 &&
-        !tilesOnP1NewHead.every((v) => v._type == apple)
+        !tilesOnP1NewHead.every(
+            (v) =>
+                v._type == apple ||
+                v._type == grass ||
+                v._type == grassWithStraws
+        )
     ) {
         clearInterval(update);
         return;
     }
-    console.log(tilesOnP1NewHead);
+    player1.parts.unshift(p1NewHead);
+    addSprite(p1NewHead[0], p1NewHead[1], p1);
 
     const tilesOnP2NewHead = getTile(p2NewHead[0], p2NewHead[1]);
     if (
         tilesOnP1NewHead.length > 0 &&
-        !tilesOnP2NewHead.every((v) => v._type == apple)
+        !tilesOnP2NewHead.every(
+            (v) =>
+                v._type == apple ||
+                v._type == grass ||
+                v._type == grassWithStraws
+        )
     ) {
         clearInterval(update);
         return;
     }
-
-    player1.parts.map((part) => {
-        clearTile(part[0], part[1]);
-    });
-    player2.parts.map((part) => {
-        clearTile(part[0], part[1]);
-    });
-
-    player1.parts.pop();
-    player1.parts.unshift(p1NewHead);
-    player2.parts.pop();
     player2.parts.unshift(p2NewHead);
+    addSprite(p2NewHead[0], p2NewHead[1], p2);
 
-    player1.parts.map((part) => {
-        addSprite(part[0], part[1], p1);
-    });
-    player2.parts.map((part) => {
-        addSprite(part[0], part[1], p2);
-    });
+    const p1Tail = player1.parts.pop();
+    const p2Tail = player2.parts.pop();
+
+    // Find out what type of grass was on the tile and replace it with the same type after clearInterval
+    const tileUnderP1Tail = getTile(p1Tail[0], p1Tail[1]);
+    const tileUnderP2Tail = getTile(p2Tail[0], p2Tail[1]);
+
+    clearTile(p1Tail[0], p1Tail[1]);
+    clearTile(p2Tail[0], p2Tail[1]);
+
+    if (tileUnderP1Tail.length > 0) {
+        addSprite(
+            p1Tail[0],
+            p1Tail[1],
+            tileUnderP1Tail.find(
+                (v) => v._type == grass || v._type == grassWithStraws
+            )._type
+        );
+    }
+    if (tileUnderP2Tail.length > 0) {
+        addSprite(
+            p2Tail[0],
+            p2Tail[1],
+            tileUnderP2Tail.find(
+                (v) => v._type == grass || v._type == grassWithStraws
+            )._type
+        );
+    }
 }, 500);
